6a1f6b65b194819a43baceda72d70e69adf9c84c
Mostafa Nasrollahpour <iammostafanasrollahpour@gmail.com>
2024-12-16 18:07:21 +0330

    make the programm functional to improve readability

--- Chess/src/main/java/org/example/Board.java
+++ Chess/src/main/java/org/example/Board.java
@@ -7,8 +7,12 @@ public class Board {
 
     final Color BLACK = new Color(16, 134, 45);
     final Color WHITE = new Color(214, 206, 185);
-    final Color INVISIBLE = new Color(0,0,0,0);
     final Image MAIN_ICON = new ImageIcon("img/icon.png").getImage();
+
+    JPanel boardPanel;
+    JLabel[][] cells; // this is the white and black cells in board
+    JButton[][] blackPieces;
+    JButton[][] whitePieces;
 
     public Board(){
         JFrame frame = new JFrame("Chess");
@@ -16,13 +20,22 @@ public class Board {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setBounds(400, 150, 800, 800);
 
-        JPanel boardPanel = new JPanel();
+        boardPanel = new JPanel();
         boardPanel.setBounds(0,0,500,500);
         boardPanel.setLayout(new GridLayout(8,8));
-        
+
+        setCells();
+        setPieces();
+
+        frame.add(boardPanel);
+        frame.setVisible(true);
+    }
+
+    private void setCells(){
+
         int colorIndex = 0;
 
-        JLabel[][] cells = new JLabel[8][8];
+        cells = new JLabel[8][8];
         for (int i = 0; i < cells.length; i++) {
             for (int j = 0; j < cells[i].length; j++) {
                 cells[i][j] = new JLabel();
@@ -31,16 +44,19 @@ public class Board {
                 else
                     cells[i][j].setBackground(BLACK);
 
-                colorIndex++;
                 cells[i][j].setOpaque(true);
+
                 boardPanel.add(cells[i][j]);
+                colorIndex++;
             }
             colorIndex++;
         }
+    }
 
+    private void setPieces(){
         //--------------- set pieces
-        JButton[][] blackPieces = new JButton[2][8];
-        JButton[][] whitePieces = new JButton[2][8];
+        blackPieces = new JButton[2][8];
+        whitePieces = new JButton[2][8];
 
         for (int i = 0; i < blackPieces.length; i++) {
             for (int j = 0; j < blackPieces[i].length; j++) {
@@ -86,8 +102,6 @@ public class Board {
         }
 
         // set pieces ---------------
-
-        frame.add(boardPanel);
-        frame.setVisible(true);
     }
+
 }
